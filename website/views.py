006827a426acc2d60a7528863f49d45f3f769217
teigorme <igorteixeira354@gmail.com>
2025-02-17 14:16:22 +0100

    add navbar,footer and carousel

--- website/views.py
+++ website/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "index.html")
+
+
 def index(request):
     return render(request, "index.html")
+
+def details(request):
+    return render(request, "details.html")
